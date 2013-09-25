0749cd3024468fca04292ad77b5fbfc00e3cd10d
Jenna <jennabantjes@gmail.com>
2013-09-25 09:34:03 -0400

    Move click functions out of resize function

--- javascripts/app.js
+++ javascripts/app.js
@@ -10,39 +10,39 @@
                 console.log('small');
                 $('#open-menu').show();
                 $('#open-menu-2').hide();
-
-                $('#open-menu').click(function(){
-                    $('#open-menu div').toggleClass('flip-vertical');
-                    $('ul.categories').slideToggle();
-                });
-
-                $('ul.categories a').click(function(){
-                    console.log('come on');
-                    $('#open-menu').hide();
-                    $('#open-menu-2').show();
-                    $('.arrow-right-nav').show();
-                    $('ul.categories').hide();
-                    $('.flyout').show();
-                });
-
-                $('.arrow-right-nav').click(function(){
-                    $('#open-menu').show();
-                    $('#open-menu-2').hide();
-                    $('ul.categories').show();
-                    $('.flyout').hide();
-                });
             }
 
             else {
                 $('#open-menu').hide();
                 $('#open-menu-2').hide();
-
-                $('ul.categories a').click(function(){
-                    $('.flyout').slideToggle();
-                });
             }
         });
-    }
+
+            $('ul.categories a').click(function(){
+                $('.flyout').slideToggle();
+            });
+
+            $('#open-menu').click(function(){
+                $('#open-menu div').toggleClass('flip-vertical');
+                $('ul.categories').slideToggle();
+            });
+
+            $('ul.categories a').click(function(){
+                console.log('come on');
+                $('#open-menu').hide();
+                $('#open-menu-2').show();
+                $('.arrow-right-nav').show();
+                $('ul.categories').hide();
+                $('.flyout').show();
+            });
+
+            $('.arrow-right-nav').click(function(){
+                $('#open-menu').show();
+                $('#open-menu-2').hide();
+                $('ul.categories').show();
+                $('.flyout').hide();
+            });
+        }
 
     swapNavs();
 
